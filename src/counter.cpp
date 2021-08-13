2d263f33004290771f9a29426ec31a0c1e0aad96
Zac Pullen <zac@greenroomrobotics.com>
2021-08-13 14:36:47 +1000

    add base ros2 code

---
+++ src/counter.cpp
@@ -0,0 +1,41 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int32.hpp"
+
+using namespace std::chrono_literals;
+
+class CounterPublisher : public rclcpp::Node
+{
+  public:
+    CounterPublisher()
+    : Node("minimal_publisher"), count_(0)
+    {
+      publisher_ = this->create_publisher<std_msgs::msg::Int32>("CounterFromROS", 10);
+      timer_ = this->create_wall_timer(
+      500ms, std::bind(&CounterPublisher::timer_callback, this));
+    }
+
+  private:
+    void timer_callback()
+    {
+      auto message = std_msgs::msg::Int32();
+      message.data = count_++;
+      RCLCPP_INFO(this->get_logger(), "Counting: '%i'", message.data);
+      publisher_->publish(message);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher_;
+    size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<CounterPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
